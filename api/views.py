a417dc92b8b1155ebb2af4400a9499c4161862fe
eric-waithaka <eric.email>
2022-06-19 17:33:19 +0300

    My first API

---
+++ api/views.py
@@ -0,0 +1,7 @@
+from rest_framework import generics
+from books.models import Books
+from .serializers import BookSerializer
+
+class BookAPIView(generics.ListAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BookSerializer
